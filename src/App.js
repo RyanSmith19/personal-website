9d224c324d4daf05f39a6f98c20f1676da582493
RyanSmith19 <lifewithryans@gmail.com>
2024-07-01 13:33:59 -0700

    added the backloading the background image

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import Projects from './components/Projects';
 import Languages from './components/Languages';
 
 const App = () => {
+  const backgroundImage = `${process.env.PUBLIC_URL}/background.jpg`;
+  
   return (
-    <div className="App">
+    <div 
+      className="App"
+      style={{ 
+        background: `url(${backgroundImage}) no-repeat center center fixed`, 
+        backgroundSize: 'cover' 
+      }}
+    >
       <About />
       <Projects />
       <Languages />
