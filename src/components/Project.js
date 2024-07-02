4cdb405af8cec40d7d2aa41f577c9e56ccc67f0f
RyanSmith19 <lifewithryans@gmail.com>
2024-07-01 17:08:35 -0700

    Project changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './Project.css';
 
-const Project = ({ title, description, link }) => {
+const Project = ({ title, description, keyFeatures, githubLink, projectLink }) => {
   return (
-    <div className="project-square">
+    <div className="project-card">
       <h3>{title}</h3>
       <p>{description}</p>
-      <a href={link} target="_blank" rel="noopener noreferrer">View on GitHub</a>
+      {keyFeatures && (
+        <ul className="key-features">
+          {keyFeatures.map((feature, index) => (
+            <li key={index}>{feature}</li>
+          ))}
+        </ul>
+      )}
+      <div className="project-links">
+        {projectLink && (
+          <a href={projectLink} target="_blank" rel="noopener noreferrer" className="project-link">
+            <i className="fas fa-external-link-alt"></i> View Project
+          </a>
+        )}
+        {githubLink && (
+          <a href={githubLink} target="_blank" rel="noopener noreferrer" className="github-link">
+            <i className="fab fa-github"></i> View on GitHub
+          </a>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,14 +4,46 @@ import './Projects.css';
 
 const projectData = [
   {
+    title: "Figg's Window Cleaning Website",
+    description: "Developed a professional website for a local window cleaning business, Figg's Window Cleaning. The site features a clean and responsive design, highlighting the company's services, team, and contact information.",
+    keyFeatures: [
+      'Responsive design ensuring optimal viewing on all devices',
+      'Detailed service descriptions',
+      'Clear contact information for easy appointment scheduling',
+    ],
+    githubLink: 'https://github.com/RyanSmith19/figg-window-cleaning',
+    projectLink: 'https://figgwindowcleaning.rf.gd'
+  },
+  {
+    title: 'Xperi Splunk Dashboards',
+    description: 'Developed a suite of Splunk Dashboards for Xperi to help data analysts understand customer behavior with their cloud nDVR service.',
+    keyFeatures: [
+      'AWS EC2 architecture for scalable deployment',
+      'Specialized queries for user pattern analysis',
+      'Creation of Splunk SPL reports',
+      'Development of comprehensive Splunk Dashboards',
+    ],
+  },
+  {
     title: 'Daily Quote Email',
-    description: 'Pulls the "Quote of the Day" from a quote website and sends it to my email.',
-    link: 'https://github.com/RyanSmith19/DailyGraceQuote'
+    description: 'Automates the process of fetching and sending a daily inspirational quote from GraceQuotes.org to your email.',
+    keyFeatures: [
+      'Fetches daily quote from GraceQuotes.org',
+      'Extracts and decodes the quote and author',
+      'Sends the quote via email',
+      'Automated daily trigger'
+    ],
+    githubLink: 'https://github.com/RyanSmith19/DailyGraceQuote',
   },
   {
     title: 'Carpool Organizing Script',
-    description: 'Algorithm that takes in a list of riders and drivers and organizes carpools to streamline process.',
-    link: 'https://github.com/RyanSmith19/BereanCarpoolRidesOrganizer'
+    description: 'Automates the organization of carpools using Google Sheets. Matches drivers and riders based on availability, location, and preferences. Outputs results into organized sheets for easy management.',
+    keyFeatures: [
+      'Matches drivers with riders',
+      'Handles unmatched riders',
+      'Updates in real-time via Google Form submissions'
+    ],
+    githubLink: 'https://github.com/RyanSmith19/BereanCarpoolRidesOrganizer',
   },
   // Add more projects as needed
 ];
@@ -26,7 +58,9 @@ const Projects = () => {
             key={index}
             title={project.title}
             description={project.description}
-            link={project.link}
+            keyFeatures={project.keyFeatures}
+            githubLink={project.githubLink}
+            projectLink={project.projectLink}
           />
         ))}
       </div>
